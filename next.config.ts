4dacbdbfcd720c3958a4c625ece48a5046378cbb
sakthii05 <ld.sakthivel05@gmail.com>
2026-01-27 15:26:40 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone', 
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-},
+  serverExternalPackages: ['sharp', 'onnxruntime-node'],
 };
 
 export default nextConfig;
